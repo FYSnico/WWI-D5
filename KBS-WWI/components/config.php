16ec130e079417aac6d3024da9cf61ae331b8638
Juliën <julien.kolkman@gmail.com>
2019-11-13 23:01:34 +0100

    Alleen username invoeren werkt, ik ga morgen verder :) ohja en conn bij config is nu pdo

--- KBS-WWI/components/config.php
+++ KBS-WWI/components/config.php
@@ -2,10 +2,10 @@
 $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
 $user = "root";
 $pass = "";
-$conn = new PDO($db, $user, $pass);
+$pdo = new PDO($db, $user, $pass);
 try{
     // display a message if connected to database successfully
-    if($conn){
+    if($pdo){
         // echo "Connected to the <strong>$db</strong> database successfully!";
     }
 }catch (PDOException $e){
