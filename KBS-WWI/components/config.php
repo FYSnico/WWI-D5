6d8dee8ea17a06e537bc886db84526758757c91e
Nico <nicolodewijks@hotmail.com>
2019-11-13 14:25:44 +0100

    Databaseverbinding gemaakt

--- KBS-WWI/components/config.php
+++ KBS-WWI/components/config.php
@@ -1,1 +1,15 @@
 <?php
+$db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+$user = "root";
+$pass = "";
+$conn = new PDO($db, $user, $pass);
+try{
+    // display a message if connected to database successfully
+    if($conn){
+        // echo "Connected to the <strong>$db</strong> database successfully!";
+    }
+}catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
+}
+?>
