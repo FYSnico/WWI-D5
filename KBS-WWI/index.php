e6bbf09c9298d50dfad97032b61b6f9d61134264
Wael <waelaf95@gmail.com>
2019-11-15 13:27:08 +0100

    homepagina -> random products genareren met (categorie, prijs, voorraad)

--- KBS-WWI/index.php
+++ KBS-WWI/index.php
@@ -15,17 +15,32 @@ include("components/config.php");
                 <?php
                     //random products genareren
                     $img = 'https://picsum.photos/200/300';
-                    $sql = "SELECT StockItemName, RecommendedRetailPrice FROM stockitems ORDER BY RAND() LIMIT 3";
+                    $sql = "SELECT StockItemName, RecommendedRetailPrice, QuantityPerOuter, StockGroupName 
+                            FROM stockitems S 
+                            JOIN stockitemstockgroups SIG 
+                            ON S.StockitemID = SIG.StockitemID
+                            JOIN stockgroups SG
+                            ON SIG.StockGroupID = SG.StockGroupID
+                            ORDER BY RAND()
+                            LIMIT 3
+                            ";
                     $result = $pdo->query($sql);
                     if($result->rowCount() > 0){
                         while($row = $result->fetch()){
-                            echo "<div class=' mb-3'>";
+                            echo "<div class=' products mb-3'>";
                                 echo "<div class='rand_products card shadow'>";
                                     echo "<img src='$img' class='card-img-top h-75' alt=''>";
                                     echo "<div class='card-body d-flex flex-column'>";
                                         echo "<h5 class='card-title'>";
                                             echo $row['StockItemName'];
                                         echo "</h5>";
+                                        echo "<div class"
+                                        echo "<p class='card-title text-primary'><strong>Categorie: </strong>";
+                                            echo $row['StockGroupName'];
+                                        echo "</p>";
+                                        echo "<p class='card-title text-warning'><strong>Voorraad: </strong>";
+                                            echo $row['QuantityPerOuter'];
+                                        echo "</p>";
                                         echo "<h5 class='card-title text-danger'>";
                                             echo $row['RecommendedRetailPrice'];
                                         echo "€</h5>";
