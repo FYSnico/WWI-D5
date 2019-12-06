642bd33cc3897d48650e5697dfd34055ab0ff260
Juliën <julien.kolkman@gmail.com>
2019-12-06 11:48:41 +0100

    Ik hou van kaas

--- KBS-WWI/product_item.php
+++ KBS-WWI/product_item.php
@@ -25,71 +25,9 @@ include("functions.php");
                 $convertRate = @convertCurrency(1, 'USD', 'EUR');
                 while ($row = $result->fetch()) {
                     echo '<aside class="col-sm-5 border-right">';
-<<<<<<< Updated upstream
-                        echo '<article class="gallery-wrap">';
-                            echo '<div class="img-big-wrap">';
-                                echo '<div> <a href="#"><img src="https://picsum.photos/460/600"></a></div>';
-                            echo '</div>';
-                        echo '</article>';
-                    echo '</aside>';
-                    echo '<aside class="col-sm-7 pb-3">';
-                        echo '<article class="card-body p-5">';
-                            echo '<h3 class="title mb-3">';
-                                echo $row['StockItemName'];
-                            echo '</h3>';
-                            echo '<p class="price-detail-wrap">';
-                                echo '<dl class="param param-inline">';
-                                    echo '<dt>';
-                                    $result = $pdo->query($sql);
-                                    while ($categories = $result->fetch()) {
-                                        echo "<a href='product.php?id=" . $categories['StockGroupID'] . "'> ";
-                                            echo $categories['StockGroupName'] . " ";
-                                        echo '</a>';
-                                    }
-                                    echo '</dt>';
-                                echo '</dl>';
-                                echo '<span class="price h3 text-warning">';
-                                    echo '<span class="currency">€</span><span class="num">';
-                                        echo round($row['RecommendedRetailPrice'] * $convertRate, 2);
-                                    echo '</span>';
-                                echo '</span>';
-                            echo '</p>';
-                            echo '<span class="">';
-                                echo '<span class=""><strong>Omschrijving: </strong>';
-                                    echo '<br>';
-                                    echo $row['SearchDetails'];
-                                echo '</span>';
-                            echo '</span>';
-                        echo '</article>';
-                        echo '<hr>';
-                    echo '<div class="card-body pt-0 pr-5 pb-0 pl-5">';
-                        echo '<div class="">';
-                            echo '<dl class="param param-inline">';
-                                echo '<dt> Voorraad: ';
-                                    echo $row['LastStockTakeQuantity'];
-                                echo '</dt>';
-                            echo '</dl>';
-                            echo '<dl class="param param-inline">';
-                                echo '<dt> Gekoeld: ';
-                                if ($row['IsChillerStock'] == 1){
-                                    echo "Ja";
-                                }else{
-                                    echo "Nee";
-                                }
-                                echo '</dt>';
-                            echo '</dl>';
-                            echo '<dl class="param param-inline">';
-                            if ($row['Size']){
-                                echo '<dt> Grootte: ';
-                                    echo $row['Size'];
-                                echo '</dt>';
-                            }
-                            echo '</dl>';
-                        echo '</div>';
-=======
                     echo '<article class="gallery-wrap">';
                     echo '<div class="img-big-wrap">';
-                    echo '<div> <a href="#"><img src="https://picsum.photos/460/500"></a></div>';
+                    echo '<div> <a href="#"><img src="https://picsum.photos/460/600"></a></div>';
                     echo '</div>';
                     echo '</article>';
                     echo '</aside>';
@@ -111,7 +49,7 @@ include("functions.php");
                     echo '</dl>';
                     echo '<span class="price h3 text-warning">';
                     echo '<span class="currency">€</span><span class="num">';
-                    echo $row['RecommendedRetailPrice'];
+                    echo round($row['RecommendedRetailPrice'] * $convertRate, 2);
                     echo '</span>';
                     echo '</span>';
                     echo '</p>';
@@ -123,8 +61,8 @@ include("functions.php");
                     echo '</span>';
                     echo '</article>';
                     echo '<hr>';
-                    echo '<div class="row">';
-                    echo '<div class="col-sm-5">';
+                    echo '<div class="card-body pt-0 pr-5 pb-0 pl-5">';
+                    echo '<div class="">';
                     echo '<dl class="param param-inline">';
                     echo '<dt> Voorraad: ';
                     echo $row['LastStockTakeQuantity'];
@@ -132,28 +70,26 @@ include("functions.php");
                     echo '</dl>';
                     echo '<dl class="param param-inline">';
                     echo '<dt> Gekoeld: ';
-                    if ($row['IsChillerStock'] == 1) {
+                    if ($row['IsChillerStock'] == 1){
                         echo "Ja";
-                    } else {
+                    }else{
                         echo "Nee";
                     }
                     echo '</dt>';
                     echo '</dl>';
                     echo '<dl class="param param-inline">';
-                    if ($row['Size']) {
+                    if ($row['Size']){
                         echo '<dt> Grootte: ';
                         echo $row['Size'];
                         echo '</dt>';
                     }
                     echo '</dl>';
                     echo '</div>';
->>>>>>> Stashed changes
                     echo '</div>';
                     echo '<hr>';
 
                     //product toevoegen in winkelmand - johan
                     echo <<<EOT
-<<<<<<< Updated upstream
                     <div class="card-body pt-0 pr-5 pb-0 pl-5">
                         <form method="POST" action="" class="">
                         <input name="hoeveel" value="1" type="number" class="btn btn-lg btn-outline-primary text-uppercase">
@@ -163,13 +99,6 @@ include("functions.php");
                     </div>
 EOT;
 
-=======
-                    <form method="POST" action="">
-                    <input name="hoeveel" value="1" type="number" class="btn btn-lg btn-outline-primary text-uppercase">
-                    <input name="id" type="text" class="d-none" value=$item>
-                    <button type="submit" name="submit" value="submit" class="btn btn-lg btn-outline-primary text-uppercase"><i class="fas fa-shopping-cart"></i> Toevoegen</button></form><br>
-EOT;
->>>>>>> Stashed changes
                     $lastStockTakeQuantity = $row['LastStockTakeQuantity'];
                     $productmagwordentoegevoegd = false;
 
@@ -194,7 +123,7 @@ EOT;
                         $hoeveel = $_POST["hoeveel"];
 
                         // starten session shoppincart
-                        if (!isset($_SESSION["shoppingcart"])) {
+                        if(!isset($_SESSION["shoppingcart"])){
                             $_SESSION["shoppingcart"] = array();
                         }
                         $shoppingcart = $_SESSION["shoppingcart"];
