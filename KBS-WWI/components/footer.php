585ca2d0b20194c41d3263d1ba567ae44a6d99bf
Johan <s1149495@student.windesheim.nl>
2019-12-11 13:45:21 +0100

    disconnect met de database

--- KBS-WWI/components/footer.php
+++ KBS-WWI/components/footer.php
@@ -2,6 +2,14 @@
 <footer id="" class="bg-dark text-white">
     <div class="container text-center p-2">
         <small>Copyright &copy; <?php echo date("Y"); ?> -  WWI-Webshop</small>
+        <?php
+        if ($mysqli){
+            $mysqli -> close();
+        }
+        if ($pdo){
+            $pdo -> close();
+        }
+        ?>
     </div>
 </footer>
 <script>
