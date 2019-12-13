120d746e4e43ffdfbc69c045f84a961e8bfd3127
Wael <waelaf95@gmail.com>
2019-12-13 15:22:31 +0100

    Banner fix

--- KBS-WWI/index.php
+++ KBS-WWI/index.php
@@ -4,7 +4,6 @@ include("components/config.php");
 include("functions.php");
 
 // Random products genareren
-
 $sql = "SELECT SG.StockGroupID, S.StockItemID, StockItemName, RecommendedRetailPrice, Photo, UnitPrice, QuantityPerOuter, StockGroupName, LastStockTakeQuantity 
 
                             FROM stockitems S 
@@ -18,56 +17,54 @@ $sql = "SELECT SG.StockGroupID, S.StockItemID, StockItemName, RecommendedRetailP
                             LIMIT 5
                             ";
 $result = $pdo->query($sql);
-echo '<img src="images/banner.jpg" alt="">';
+?>
+<div class="container">
+    <img class="w-100 text-center" src="images/banner.jpg" alt="">
+    <div class="content">
+        <h3>Ontdek onze winkel</h3>
+        <br>
+        <?php
+        //Currency converter
+        $convertRate = @convertCurrency2(1, 'USD', 'EUR');
+        // Random products weergeven
+        if ($result->rowCount() > 0) {
 
-
-    ?>
-    <div class="container">
-        <div class="content">
-            <h3>Ontdek onze winkel</h3>
-            <br>
-            <?php
-            //Currency converter
-            $convertRate = @convertCurrency2(1, 'USD', 'EUR');
-            // Random products weergeven
-            if ($result->rowCount() > 0) {
-
-                echo "<div class=\"card-deck kaartdeck\">";
-                    while ($row = $result->fetch()) { ?>
-                        <div class="card w-25 kaartbreedte" style="width: 18rem;">
-                            <a href='product_item.php?id="<?php echo $row['StockItemID'] ?>"'>
-                                <?php
-                                if ($row['Photo']) {
-                                    echo '<img class="card-img-top kaartimg" src="data:image/jpeg;base64,' . base64_encode($row['Photo']) . '"/>';
-                                } else {
-                                    echo '<img class="card-img-top kaartimg" src="images/default-product.png" alt="">';
-                                }
-                                ?>
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title kaarttitel"><a
-                                            href='product_item.php?id="<?php echo $row['StockItemID'] ?>"'><?php echo $row['StockItemName']; ?></a>
-                                </h5>
-                            </div>
-                            <div class="card-footer kaartfooter">
-                                <p class='card-text text-primary'><a
-                                            href='product.php?id="<?php echo $row['StockGroupID'] ?>"'><?php echo $row['StockGroupName'] ?></a>
-                                </p>
-                                <p class='card-text text-warning'><?php echo $row['LastStockTakeQuantity'] ?> stocks op voorraad</p>
-                                <p class="card-text">
-                                    €<?php $UnitPrice = $row['UnitPrice'] * $convertRate;
-                                echo number_format($UnitPrice,2,",",".") ?></p>
-                            </div>
+            echo "<div class=\"card-deck kaartdeck\">";
+                while ($row = $result->fetch()) { ?>
+                    <div class="card w-25 kaartbreedte" style="width: 18rem;">
+                        <a href='product_item.php?id="<?php echo $row['StockItemID'] ?>"'>
+                            <?php
+                            if ($row['Photo']) {
+                                echo '<img class="card-img-top kaartimg" src="data:image/jpeg;base64,' . base64_encode($row['Photo']) . '"/>';
+                            } else {
+                                echo '<img class="card-img-top kaartimg" src="images/default-product.png" alt="">';
+                            }
+                            ?>
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title kaarttitel"><a
+                                        href='product_item.php?id="<?php echo $row['StockItemID'] ?>"'><?php echo $row['StockItemName']; ?></a>
+                            </h5>
                         </div>
-                    <?php } ?>
-                </div>
-                <?php
-                unset($result);
-            } else {
-                echo "Geen producten gevonden.";
-            }
-            ?>
-        </div>
+                        <div class="card-footer kaartfooter">
+                            <p class='card-text text-primary'><a
+                                        href='product.php?id="<?php echo $row['StockGroupID'] ?>"'><?php echo $row['StockGroupName'] ?></a>
+                            </p>
+                            <p class='card-text text-warning'><?php echo $row['LastStockTakeQuantity'] ?> stocks op voorraad</p>
+                            <p class="card-text">
+                                €<?php $UnitPrice = $row['UnitPrice'] * $convertRate;
+                            echo number_format($UnitPrice,2,",",".") ?></p>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <?php
+            unset($result);
+        } else {
+            echo "Geen producten gevonden.";
+        }
+        ?>
     </div>
-    <br><br>
+</div>
+<br><br>
 <?php include('components/footer.php') ?>
